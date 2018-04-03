279feec1f37215d644a17e4354ac50f155fd11f1
autodocs <autodocs>
2018-04-03 15:36:59 +0000

    build based on fd22daf

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.17",
   "release-0.16",
   "release-0.15",
   "release-0.14",
@@ -15,6 +16,7 @@ var DOC_VERSIONS = [
   "v0.5.1",
   "v0.5.0",
   "v0.4.5",
+  "v0.17.0",
   "v0.16.3",
   "v0.16.1",
   "v0.15.0",
